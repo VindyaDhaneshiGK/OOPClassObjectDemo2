b0c1e56e1af533ab92617f8a8274755fc8e17ae2
Vindya Gardiyawasam <vindyadgardiyawasam@outlook.com>
2025-10-06 13:54:03 +0530

    Add project files.

---
+++ OOPClassObjectDemo2/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace OOPClassObjectDemo2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
