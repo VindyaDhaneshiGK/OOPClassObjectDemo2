3cc2a7645226c3f35095076812d7344c07dbbf63
Vindya Gardiyawasam <vindyadgardiyawasam@outlook.com>
2025-10-06 13:59:32 +0530

    commit 3

---
+++ OOPClassObjectDemo2/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPClassObjectDemo2
+{
+    public class Person
+    {
+        // Define the Name property
+        public string Name { get; set; }
+
+        // Define the Age property
+        public int Age { get; set; }
+
+        // Define the Greet method
+        public void Greet()
+        {
+            Console.WriteLine("Hello, my name is " + Name);
+        }
+    }
+}
